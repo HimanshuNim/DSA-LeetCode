fe02cf750954179083a53589541e01d7111f0116
Himanshu <himanshunimrad@gmail.com>
2026-08-18 15:09:37 +0530

    Time: 0 ms (100%), Space: 47.1 MB (92.42%) - CodeHub

---
+++ leetcode/0268-missing-number/0268-missing-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int xorSum=nums.length;
+        for(int i=0;i<nums.length;i++){
+            xorSum^=i^nums[i];
+        }
+        return xorSum;
+    }
+}
